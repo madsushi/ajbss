fde3fd03c5ee365e59a4014c715f5c4543168e46
Chase Christian <madsushi@gmail.com>
2017-12-29 23:53:44 -0800

    updated readme and added comments
    Added more documentation, via README.md and lots of inline comments

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-SECRET SANTA BOT
+# Secret Santa Bot
+
+This is a tool to facilitate Secret Santa sorting and recordkeeping across years.
+
+Here are the core features:
+
+ * Keep track of historic SS data
+ * Allow people to sign up for SS
+ * Match people based on criteria
+ * Provide a way for people to update their SS status
+ * Admins should be able to manage SS
+
+Here's some table definitions:
+
+*users*:
+```user_id // user_name // status // type ```
+
+```status``` in this table refers to ban/auth status.
+
+*pairings*:
+``` pairing_id // year / type / santa / santee / status ```
+
+```status``` in this table refers to completion status (incomplete, sent, received, etc).
+
--- sort.php
+++ sort.php
@@ -11,74 +11,111 @@
       $dir = 'sqlite:ajbss.db';
       $dbh = new PDO($dir) or die("can't talk to SQLite");
 
-      ## query to retrieve list of users, this needs to be more strict (only users that signed up, etc)
-      $user_id_query = "SELECT user_id FROM users WHERE status=0 limit 10";
+      ## DB query to retrieve list of users, this needs to be more strict (only users that signed up, etc)
+      ## limit is in place to make finding errors/failures faster
+      ## should we have different users per year/type? otherwise how do we check if they're signed up THIS year?
+      $get_santas_query = "SELECT user_id FROM users WHERE status=0 limit 10";
 
-      ## query to retrieve list of pairings
-      $pairing_query = "SELECT santa, santee, year FROM pairings where type = ?";
+      ## DB query to retrieve list of exisitng pairings, for exclusion from new pairings
+      $get_previous_pairings_query = "SELECT santa, santee, year FROM pairings where type = ?";
 
-      ## query to add pairings
-      $add_pairing_query = "INSERT INTO pairings (year, type, santa, santee, status) VALUES (?,?,?,?,?)";
-      $add_pairing = $dbh->prepare($add_pairing_query);
+      ## DB query to add pairings once a complete match set is created
+      ## prepare this one in advance since we'll call it multiple times within a loop
+      $add_new_pairings_query = "INSERT INTO pairings (year, type, santa, santee, status) VALUES (?,?,?,?,?)";
+      $add_new_pairings = $dbh->prepare($add_new_pairings_query);
 
-      ## set some defaults, these would be selected by admins
+      ## set/get some defaults, these would be selected by admins
       $year = $_GET["year"];
       $type = $_GET["type"];
-      $pairing_array = [];
+      
+      ## $previous_pairings is used to hold the previous pairings we get back from the database in an easy-to-use format
+      $previous_pairings = [];
+      
+      ## $santas_master_list is where we hold potential santas, and DO NOT MODIFY IT, in case we have to reset and try again
+      $santas_master_list = [];
+      
+      ## $to_be_insterted is where we put our pairings for INSERT once it's completed
+      $to_be_inserted = [];
+      
+      ## we start with $status at 0 since the pairings are new, this will be incremented by user and admin pages
       $status = 0;
-      $user_id_list = [];
-      $insert_array = [];
+      
+      ## this is a throwaway variable for the next-year links
       $next_year = $year + 1;
 
-      echo "<p><a href='/ajbss/sort.php?year=" . $next_year . "&type=" . $type . "'>NEXT YEAR</a></p><p><a href='/ajbss/delete.php'>DELETE DATA (no worries)<
-/a></p>";
+      ## throwaway links for re-running multiple years and wiping data for testing
+      echo "<p><a href='/ajbss/sort.php?year=" . $next_year . "&type=" . $type . "'>NEXT YEAR</a></p><p><a href='/ajbss/delete.php'>DELETE DATA (no worries)</a></p>";
 
-      ## retrieve the data from SQLite and initialize variable
-      $user_id_results = $dbh->query($user_id_query)->fetchAll(PDO::FETCH_COLUMN);
+      ## retrieve the list of santas from SQLite and initialize variable, we're only grabbing their ID, so we use FETCH_COLUMN
+      $get_santas_results = $dbh->query($get_santas_query)->fetchAll(PDO::FETCH_COLUMN);
 
-      foreach($user_id_results as $value) {
+      ## set the key = value for the $santas_list array, so that we can easily punch stuff out
+      foreach($get_santas_results as $value) {
 
-        $user_id_list[$value] = $value;
+        $santas_master_list[$value] = $value;
 
       }
 
       ## shuffle the user list, this is where part of our entropy comes from
       ## shuffle($user_id_list);
       ## this doesn't work right, since $key != $value anymore
+      ## the foreach($get_santas_results) list needs to have keys match values for easy access
 
-      ## make two copies of the list, so that we can punch out eacn individually
-      $santa_list = $user_id_list;
-      $santee_list = $user_id_list;
+      ## make a copy of the $santas_list, so that we can punch users out of each list (santas, santees) individually
+      ## these are the list we can modify
+      $santa_list = $santas_master_list;
+      $santee_list = $santas_master_list;
+      
 
-      ## retrieve list of pairings
-      $pairing_list_query = $dbh->prepare($pairing_query);
-      $pairing_list_query->execute([$type]);
-      $pairing_list = $pairing_list_query->fetchAll();
+      ## retrieve list of previous pairings, put it into one big array
+      $previous_pairings_list_query = $dbh->prepare($get_previous_pairings_query);
+      $previous_pairings_list_query->execute([$type]);
+      $previous_pairings_list = $previous_pairings_list_query->fetchAll();
 
-      foreach($pairing_list as list($a, $b, $c)) {
+      ## the $previous_pairings_array is based on [santa][year] = santee, so we can see ALL of the pairings for each santa
+      ## if we want to filter out people from having a previous santa as their santee, we'll have to do this array twice?
+      foreach($previous_pairings_list as list($a, $b, $c)) {
 
-        $pairing_array[$a][$c] = $b;
+        $previous_pairings[$a][$c] = $b;
 
       }
 
-      ## this punches out $santa_list
+      ## pull santas one by one from the big list of signed-up santas
       foreach($santa_list as $key=>$santa) {
+          
+        ## throwaway for showing each santa's previous pairings and new pairing
         echo "Santa: " . $santa . "<br>";
 
-
+        ## we create a temporary copy of the remaining $santee_list members
+        ## and punch out everyone who isn't eligible
         $candidate_list = $santee_list;
-        ## can't match with yourself!
+        
+        ## FILTER SECTION:
+        ## can't match with yourself, so we punch out the santa themselves
         unset($candidate_list[$key]);
+        
         ## check for previous pairings...
-        foreach($pairing_array[$santa] as $key_2=>$santee) {
+        ## throwaway to echo the years / santees of past pairings for this santa, just for visual review
+        foreach($previous_pairings[$santa] as $key_2=>$santee) {
 
+          ## punch out previous santees
           unset($candidate_list[$santee]);
           echo "OLD Year: " . $key_2 . " | Santa ID: " . $santa . " | Santee ID: " . $santee . "<br>";
 
         }
+        
         ## other filters go here...
+        ## maybe previous santas of this santa, etc
+        
+        ## randomly pull a remaining user from the $candidate_list
+        ## this is the most naive part of pairing
+        ## it's a greedy algorithm, and may not leave viable candidates for future santas
         $candidate = array_rand($candidate_list, 1);
 
+        ## if the candidate list is empty, just error and bail out
+        ## this is throwaway
+        ## what we SHOULD do is just re-roll automatically
+        ## basically just reset $santas_list and reset $santee_list back to $santas_master_list
         if(!isset($candidate)) {
 
           echo "<h1><font color=red>CANDIDATE RNG FAILED, TRY AGAIN</font></h1>";
@@ -86,23 +123,28 @@
 
         }
 
+        ## if we have a viable candidate:
         ## punch out the $santee_list
         unset($santee_list[$candidate]);
 
         ## throwaway to display / check code
         echo "<b><font color=green>NEW Year: " . $year . " | Santa ID: " . $santa . " | Santee ID: " . $candidate . "</font></b><br><br>";
 
-        $insert_array[$santa]["year"] = $year;
-        $insert_array[$santa]["type"] = $type;
-        $insert_array[$santa]["santa"] = $santa;
-        $insert_array[$santa]["candidate"] = $candidate;
-        $insert_array[$santa]["status"] = $status;
+        ## store the pairing in the $to_be_inserted holder
+        ## we will only run the INSERT command if we have a complete pairing (all santas/santees paired)
+        $to_be_inserted[$santa]["year"] = $year;
+        $to_be_inserted[$santa]["type"] = $type;
+        $to_be_inserted[$santa]["santa"] = $santa;
+        $to_be_inserted[$santa]["candidate"] = $candidate;
+        $to_be_inserted[$santa]["status"] = $status;
 
       }
 
-      foreach($insert_array as $insert) {
+      ## if we exit the main loop succesfully, go ahead and insert all the pairings
+      ## we need to add some controls to check for existing pairings for the target year first, so we don't overwrite/duplicate
+      foreach($to_be_inserted as $insert) {
 
-        $add_pairing->execute([$insert["year"],$insert["type"],$insert["santa"],$insert["candidate"],$insert["status"]]);
+        $add_new_pairings->execute([$insert["year"],$insert["type"],$insert["santa"],$insert["candidate"],$insert["status"]]);
 
       }
 
